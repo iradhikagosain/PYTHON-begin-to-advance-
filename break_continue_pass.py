134f099277fbb63b0ea5cf3da5614a864f1d0256
Radhika Gosain <iradhikagosain@gmail.com>
2025-08-28 21:17:09 +0530

    Add files via upload

---
+++ break_continue_pass.py
@@ -0,0 +1,18 @@
+#BREAK,CONTINUE AND PASS STATEMENTS
+
+
+for i in range(0,21):
+    print(i)
+    if i==15:
+        break#stop execution
+
+for j in range(21,31):
+    print(j)
+    if j==28:
+        continue#continue loop for next interation
+
+
+for k in range(31,41):
+    print(k)
+    if j==37:
+        pass
---
+++ variables_datatypes.py
@@ -0,0 +1,17 @@
+#variables and datatypes
+name="Radhika gosain"
+age=20
+cgpa=8.7
+print(name)#string
+print(age)#integer
+print(cgpa)#float
+
+AGE=24
+print(AGE)
+
+print(type(cgpa))
+
+
+is_comp=True#boolean
+comp=3+8j
+print(type(comp))#complex
